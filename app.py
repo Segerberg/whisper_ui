54d977b4ec6f9cec0a970915e1c62a75b2643b64
arch <andreassegerberg@gmail.com>
2023-09-12 13:06:51 +0200

    Final draft

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ migrate = Migrate(app, db)
 def make_celery(app):
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'],
                     backend=app.config['CELERY_RESULT_BACKEND'])
-    # celery.conf.update(app.config)
     TaskBase = celery.Task
 
     class ContextTask(TaskBase):
